94f874eea6b3b70fbe8dc57c85742ef3f8536390
binhduongxyz <85275129+binhduongxyz@users.noreply.github.com>
2021-06-03 13:04:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Đào Thị Vân Anh ơiiii!',
+    introDesc: `Sau đây là vài câu hỏi khảo sát khá là dễ
+    Em làm đi nhé :)) `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Em yêu anh bao nhiêu % 😙',
+    desc: ' Chọn đúng mới được đó nhaa ',
+    btnYes: 'Em yêu anh nhất trên đời 100% <33',
+    btnNo: 'Em yêu anh 30% thôi à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho ĐVBD <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nắmm 😘😘',
+    messDesc: 'Tối nay 10h vào discord choa anh xem mặt em nhé bae.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
